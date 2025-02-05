8dcc77fe11d605fb572589e54f3715dafbf66d39
Abdelrahmanm22 <abdra1396@gmail.com>
2025-02-05 20:11:01 +0200

    create 3 entities product,productbrand, producttype

--- Talabat.Core/obj/Debug/net6.0/Talabat.Core.AssemblyInfo.cs
+++ Talabat.Core/obj/Debug/net6.0/Talabat.Core.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Talabat.Core")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b72502b3e038ad6d037fae7420add7e3d87fe209")]
 [assembly: System.Reflection.AssemblyProductAttribute("Talabat.Core")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Talabat.Core")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
