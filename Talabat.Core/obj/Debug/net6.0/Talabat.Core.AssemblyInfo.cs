7c876f0e041af47647605694288598b7e39202e2
Abdelrahmanm22 <abdra1396@gmail.com>
2025-03-03 11:59:56 +0200

    Order Service - Interface

--- Talabat.Core/obj/Debug/net6.0/Talabat.Core.AssemblyInfo.cs
+++ Talabat.Core/obj/Debug/net6.0/Talabat.Core.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Talabat.Core")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d5813f5b5716ff1f693a368ac8f4efe2bf855de6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9e270eb10f959fcc4b6203927a6d26ca81e24ce9")]
 [assembly: System.Reflection.AssemblyProductAttribute("Talabat.Core")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Talabat.Core")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
