a27f1a02cf3cccb95120110eb0270cc00d359a08
Abdelrahmanm22 <abdra1396@gmail.com>
2025-03-04 22:34:11 +0200

    Get Delivery Methods EndPoint

--- Talabat.APIs/Controllers/OrdersController.cs
+++ Talabat.APIs/Controllers/OrdersController.cs
@@ -5,6 +5,7 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Talabat.APIs.DTOs;
 using Talabat.APIs.Errors;
+using Talabat.Core;
 using Talabat.Core.Entities.Order_Aggregate;
 using Talabat.Core.Services;
 
@@ -15,23 +16,25 @@ namespace Talabat.APIs.Controllers
     {
         private readonly IOrderService _orderService;
         private readonly IMapper _mapper;
+        private readonly IUnitOfWork _unitOfWork;
 
-        public OrdersController(IOrderService orderService,IMapper mapper)
+        public OrdersController(IOrderService orderService, IMapper mapper,IUnitOfWork unitOfWork)
         {
             this._orderService = orderService;
             this._mapper = mapper;
+            this._unitOfWork = unitOfWork;
         }
         //create order
         [HttpPost]
         [Authorize]
-        [ProducesResponseType(typeof(Order),StatusCodes.Status200OK)]
+        [ProducesResponseType(typeof(Order), StatusCodes.Status200OK)]
         [ProducesResponseType(typeof(ApiResponse), StatusCodes.Status400BadRequest)]
         public async Task<ActionResult<Order>> CreateOrder(OrderDto orderDto)
         {
             var BuyerEmail = User.FindFirstValue(ClaimTypes.Email);
             var MappedAddress = _mapper.Map<AddressDto, Address>(orderDto.ShippingAddress);
-            var order = await _orderService.CreateOrderAsync(BuyerEmail,orderDto.BasketId,orderDto.DeliveryMethodId,MappedAddress);
-            if (order is null) return BadRequest(new ApiResponse(400,"There is a problem with your order"));
+            var order = await _orderService.CreateOrderAsync(BuyerEmail, orderDto.BasketId, orderDto.DeliveryMethodId, MappedAddress);
+            if (order is null) return BadRequest(new ApiResponse(400, "There is a problem with your order"));
             return Ok(order);
         }
 
@@ -39,7 +42,8 @@ namespace Talabat.APIs.Controllers
         [HttpGet]
         [ProducesResponseType(typeof(IReadOnlyList<OrderToReturnDto>), StatusCodes.Status200OK)]
         [ProducesResponseType(typeof(ApiResponse), StatusCodes.Status404NotFound)]
-        public async Task<ActionResult<IReadOnlyList<OrderToReturnDto>>> GetOrdersForUser() {
+        public async Task<ActionResult<IReadOnlyList<OrderToReturnDto>>> GetOrdersForUser()
+        {
             var BuyerEmail = User.FindFirstValue(ClaimTypes.Email);
             var Orders = await _orderService.GetOrdersForSpecificUserAsync(BuyerEmail);
             if (Orders is null) return NotFound(new ApiResponse(404, "There is no Orders for this user."));
@@ -59,5 +63,16 @@ namespace Talabat.APIs.Controllers
             var MappedOrder = _mapper.Map<Order, OrderToReturnDto>(Order);
             return Ok(Order);
         }
+
+
+        [HttpGet("DeliveryMethod")]
+        public async Task<ActionResult<IReadOnlyList<DeliveryMethod>>> GetDeliveryMethods()
+        {
+            var Deliverymethods = await _unitOfWork.Repository<DeliveryMethod>().GetAllAsync();
+            return Ok(Deliverymethods);
+        }
     }
 }
+
+
+
